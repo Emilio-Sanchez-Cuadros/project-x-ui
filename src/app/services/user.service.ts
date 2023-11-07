f8ef5978f5dd61928576c3dd11fd04df24da2c40
Sandra <esancheznumero1@gmail.com>
2023-11-08 00:17:43 +0100

    initial commit with basic app

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  // getUsers(): Observable<User[]> {
+  //   const users = of(HEROES);
+  //   return heroes;
+  // }
+
+}
